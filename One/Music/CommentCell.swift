94abd86a7bee821edd1b8002f9f659455501ac34
胡永红 <yonghong.hu@weimob.com>
2016-09-08 18:27:57 +0800

    音乐 / section header view

---
+++ One/Music/CommentCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentCell.swift
+//  One
+//
+//  Created by Hu on 16/9/8.
+//  Copyright © 2016年 redye. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
